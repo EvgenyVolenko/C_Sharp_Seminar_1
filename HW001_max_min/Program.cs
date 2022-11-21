f5d81009254f827b0c1e61cf1ec00750759a4959
Evgeny <evolenko@inbox.ru>
2022-11-21 23:16:04 +0300

    Add program min_max

---
+++ HW001_max_min/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Write("Введите число а: ");
+string bufa = Console.ReadLine();
+int a = int.Parse(bufa);
+Console.Write("Введите число b: ");
+string bufb = Console.ReadLine();
+int b = int.Parse(bufb);
+
+int max = a;
+int min = b;
+
+if (a > b) 
+{
+    max = a;
+    min = b;
+}
+if (a < b) 
+{
+    max = b;
+    min = a;
+}
+
+Console.Write("max = ");
+Console.WriteLine(max);
+
+Console.Write("min = ");
+Console.WriteLine(min);
