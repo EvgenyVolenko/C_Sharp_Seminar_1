d78f5b49140a6a5ea7334bde52628d5514d6d16e
Evgeny <evolenko@inbox.ru>
2022-11-21 23:35:20 +0300

    Add Example chet/nechet

---
+++ HW003_%_2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число: ");
+string bufa = Console.ReadLine();
+int a = int.Parse(bufa);
+
+if (a % 2 == 0)
+{
+    Console.Write("Да. Введенное вами число четное!");
+}
+else Console.Write("Нет. Введенное вами число нечетное!");
