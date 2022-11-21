c507619c10f7b4056b6cb88c22e615bef282cc59
Evgeny <evolenko@inbox.ru>
2022-11-21 23:46:43 +0300

    Add Example Chetnie

---
+++ HW004_Chetnie/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число: ");
+string bufn = Console.ReadLine();
+int n = int.Parse(bufn);
+
+count = 1;
+
+while (count <= n)
+{
+    if (n % 2 == 0)
+    {
+        Console.Write("Да. Введенное вами число четное!", n);
+        count++;
+    }
+    else count++;
+}
