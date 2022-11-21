998d87c22bd1138dee5b602aeab904a659c030c9
Evgeny <evolenko@inbox.ru>
2022-11-21 23:29:24 +0300

    Add Example max from 3

---
+++ HW002_max_of_3/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите число а: ");
+string bufa = Console.ReadLine();
+int a = int.Parse(bufa);
+Console.Write("Введите число b: ");
+string bufb = Console.ReadLine();
+int b = int.Parse(bufb);
+Console.Write("Введите число c: ");
+string bufc = Console.ReadLine();
+int c = int.Parse(bufc);
+
+int max = a;
+
+if (b > a) max = b;
+if (max < c) max = c;
+
+Console.Write("max = ");
+Console.WriteLine(max);
